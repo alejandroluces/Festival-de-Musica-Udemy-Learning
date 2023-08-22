22278671281e01dd6c03586df0bbdfb678b943fb
Alejandro Luces <130524077+alejandroluces@users.noreply.github.com>
2023-08-22 16:04:47 -0400

    Add_Modif_Script_pjson

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
-
-const {src,dest}= require('gulp') // Importar gulp
+const {src,dest,watch}= require('gulp') // Importar gulp
 const sass = require('gulp-sass')(require('sass')) // Importar el compilador de sass
 
 function css(done) {
   
-    src('src/scss/app.scss')//Identificar el archivo fuente
+    src("src/scss/app.scss")//Identificar el archivo fuente
     .pipe(sass())//Obtener el archivo fuente. Compilador
-    .pipe(dest('build/css'));//Destino del archivo compilado para almacenar en el disco duro
+    .pipe(dest("build/css"));//Destino del archivo compilado para almacenar en el disco duro
 
   done()// Callback que avisa que la tarea ha terminado
 }
 
 function dev(done) {
-  watch('src/scss/app.scss', css);
+  watch("src/scss/app.scss", css);
   done();
 }
 
